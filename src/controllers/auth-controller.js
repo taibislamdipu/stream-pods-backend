71ac4da6fbc555f64d576c1b6c9604b60e24d825
taibislamdipu <mailtaibislam@gmail.com>
2023-06-20 03:13:08 +0600

    API: Login api add.

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -42,3 +42,39 @@ exports.register = async (req, res) => {
     res.json({ message: "Error registering user", error: error.message });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if email is taken
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ error: "Invalid email or password." });
+    }
+
+    // 4. compare password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.json({ error: "Invalid email or password" });
+    }
+
+    // 5. create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    // 6. send response
+    res.json({
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      token,
+    });
+  } catch (error) {
+    res.json({ message: "Error logging in user", error: error.message });
+  }
+};
